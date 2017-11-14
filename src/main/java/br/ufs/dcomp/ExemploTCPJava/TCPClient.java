621fdb5c4119fe44ca596821d01354fd597ba66f
Tarcisio <tarcisio@dcomp.ufs.br>
2017-11-14 00:38:50 +0000

    primeiro commit

---
+++ src/main/java/br/ufs/dcomp/ExemploTCPJava/TCPClient.java
@@ -0,0 +1,14 @@
+package br.ufs.dcomp.TCPExample;
+
+import java.net.*;
+import java.io.*;
+public class TCPClient{
+    public static void main(String[] args){
+        System.out.println("Init...");
+        try {
+            Socket sock = new Socket("127.0.0.1", 3300);
+            OutputStream os = sock.getOutputStream();
+            os.write("oiiii".getBytes());
+        }catch(Exception e){System.out.println(e);}    
+    }
+}
---
+++ src/main/java/br/ufs/dcomp/ExemploTCPJava/TCPServer.java
@@ -0,0 +1,21 @@
+package br.ufs.dcomp.TCPExample;
+
+import java.net.*;
+import java.io.*;
+public class TCPServer{
+    public static void main(String[] args){
+        System.out.println("Server Init...");
+        try {
+            ServerSocket ss = new ServerSocket(3300, 5, InetAddress.getByName("127.0.0.1"));
+            Socket sock = ss.accept();
+            InputStream is = sock.getInputStream();
+            OutputStream os = sock.getOutputStream();
+            
+            byte[] buf = new byte[20];
+            is.read(buf);
+            String msg = new String(buf);
+            System.out.println(msg);
+            
+        }catch(Exception e){System.out.println(e);}    
+    }
+}
